0173a5fb8978d617d6ea410d8d4002fbe245f738
dautord <dauitordecilla@gmail.com>
2023-03-03 16:14:13 +0800

    session_unset type void to bool

--- dtr_panel/dtr_dashboard/logout.php
+++ dtr_panel/dtr_dashboard/logout.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	session_destroy();
-	session_unset($_SESSION['admin_id']);
+	// session_unset($_SESSION['admin_id']);
+	session_unset();
 	header('location:../index.php');
 ?>
