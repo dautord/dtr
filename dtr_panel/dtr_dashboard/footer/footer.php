5fe4e79d6b9b2c43b3595af7a1edaf8b9600d1c1
dautord <dauitordecilla@gmail.com>
2023-03-14 14:04:16 +0800

    dtr to CDBS HRMS (header and footer)

--- dtr_panel/dtr_dashboard/footer/footer.php
+++ dtr_panel/dtr_dashboard/footer/footer.php
@@ -1,6 +1,5 @@
   <footer class="main-footer">
-    <strong>Copyright &copy; <?php echo date('Y');?> <a href="#">DTR Management System</a>.</strong>
-    All rights reserved.
+    <strong><?php echo date('Y');?> <a href="#">CDBS HR Management System</a>.</strong>
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
     </div>
