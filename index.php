24187175787af5238d61ebecce68a2b0f8dccc45
dautord <dauitordecilla@gmail.com>
2023-04-11 15:08:39 +0800

    11/04/23

--- index.php
+++ index.php
@@ -16,13 +16,13 @@ error_reporting(E_ALL);
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
-    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script type="text/javascript" src="instascan.min.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
-
+    <script src="script.js"></script>
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
     <script>
@@ -145,7 +145,6 @@ error_reporting(E_ALL);
       document.oncontextmenu = document.body.oncontextmenu = function() {return false;}//disable right click
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
 </body>
 
 </html>
