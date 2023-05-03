9f7044aa20769d111a2cbcc9f8324395f4c88c24
dautord <dauitordecilla@gmail.com>
2023-05-03 12:10:59 +0800

    03/05/23 connection.php changes

--- init/model/config/connection.php
+++ init/model/config/connection.php
@@ -1,7 +1,7 @@
 <?php
 	define('db_host', '192.168.128.57');
-	// define('db_host', 'localhost');
 	define('db_user', 'admin');
+	// define('db_host', 'localhost');
 	// define('db_user', 'root');
 	define('db_pass', '');
 	define('db_name', 'dtr_system');
