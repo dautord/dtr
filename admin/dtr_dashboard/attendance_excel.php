a49e19524857a75e6233d71d41fae9f11de5dd1b
dautord <dauitordecilla@gmail.com>
2023-05-10 13:19:04 +0800

    10/5/23 csv test

--- admin/dtr_dashboard/attendance_excel.php
+++ admin/dtr_dashboard/attendance_excel.php
@@ -1,58 +1,27 @@
 <?php
-	header("Content-Type: application/xlsx");    
-  header("Content-Disposition: attachment; filename=attendance_report.xlsx");  
-	header("Pragma: no-cache"); 
-	header("Expires: 0");
-
+	header('Content-Type: text/csv');
+	header('Content-Disposition: attachment; filename=attendance_report.csv');
+	
 	require('../../init/model/config/connection2.php');
 	
-	$output = "";
+	$output = fopen('php://output', 'w');
 	
-	if(ISSET($_POST['export']) ){
-		$output .="
-			<table>
-				<thead>
-					<tr>
-                    <th>QR Code</th>
-                    <th>EmployeeID No.</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Time In</th>
-                    <th>Time Out</th>
-                    <th>Log Date</th>    
-					</tr>
-				<tbody>
-		";
+	if(ISSET($_POST['export'])){
+		fputcsv($output, array('QR Code', 'EmployeeID No.', 'First Name', 'Last Name', 'Time In', 'Time Out', 'Log Date'));
 		
-		$date1 = date("Y-m-d", strtotime($_POST['date1']));
-		$date2 = date("Y-m-d", strtotime($_POST['date2']));
+		$date1 = date('Y-m-d', strtotime($_POST['date1']));
+		$date2 = date('Y-m-d', strtotime($_POST['date2']));
 
 		$stmt = $conn->prepare("SELECT * FROM tbl_attendance a INNER JOIN tbl_employee b ON a.employee_qrcode = b.qr_code WHERE a.logdate BETWEEN ? AND ? ORDER BY a.attendance_id ASC");
-		$stmt->bind_param("ss", $date1, $date2);
+		$stmt->bind_param('ss', $date1, $date2);
 	    $stmt->execute();
         $result = $stmt->get_result();
-			while($fetch = $result->fetch_assoc()){
-			
-		$output .= "
-					<tr>
-						<td>".$fetch['qr_code']."</td>
-						<td>".$fetch['employee_idno']."</td>
-					    <td>".$fetch['first_name']."</td>
-						<td>".$fetch['last_name']."</td>
-					    <td>".$fetch['time_in']."</td>
-						<td>".$fetch['time_out']."</td>
-						<td>". date("M d, Y",htmlentities(strtotime($fetch['logdate'])))."</td>
-					</tr>
-		          ";
+		
+		while($fetch = $result->fetch_assoc()){
+			fputcsv($output, array($fetch['qr_code'], $fetch['employee_idno'], $fetch['first_name'], $fetch['last_name'], $fetch['time_in'], $fetch['time_out'], date('M d, Y', strtotime($fetch['logdate']))));
 		}
 		
-		$output .="
-				</tbody>
-				
-			</table>
-		";
-		
-		echo $output;
+		fclose($output);
+		exit();
 	}
-	
 ?>
--- admin/dtr_dashboard/attendance_excel_generated.php
+++ admin/dtr_dashboard/attendance_excel_generated.php
@@ -1,30 +1,16 @@
 <?php
-    header("Content-Type: application/xlsx");
-    header("Content-Disposition: attachment; filename=attendance_report.xlsx");
-    header("Pragma: no-cache");
-    header("Expires: 0");
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename=attendance_report.csv');
+    header('Pragma: no-cache');
+    header('Expires: 0');
 
     require('../../init/model/config/connection2.php');
 
     $output = "";
 
     if(ISSET($_POST['export'])) {
-        // Initialize the output with the table headers
-        $output .= "
-            <table>
-                <thead>
-                    <tr>
-                        <th>QR Code</th>
-                        <th>EmployeeID No.</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Time In</th>
-                        <th>Time Out</th>
-                        <th>Log Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-        ";
+        // Initialize the output with the column headers
+        $output .= "QR Code,EmployeeID No.,First Name,Last Name,Time In,Time Out,Log Date\n";
 
         // Get the date range from the form
         $date1 = date("Y-m-d", strtotime($_POST['date1']));
@@ -32,19 +18,27 @@
 
         // Select the minimum time in and maximum time out for each employee and day
         $stmt = $conn->prepare("
-            SELECT 
-                a.employee_qrcode, 
-                b.employee_idno, 
-                b.first_name, 
-                b.last_name, 
-                MIN(a.time_in) AS min_time_in, 
-                MAX(a.time_out) AS max_time_out, 
-                a.logdate
-            FROM tbl_attendance a 
-            INNER JOIN tbl_employee b ON a.employee_qrcode = b.qr_code 
-            WHERE a.logdate BETWEEN ? AND ? 
-            GROUP BY a.employee_qrcode, a.logdate
-            ORDER BY a.attendance_id ASC");
+        WITH RECURSIVE dates AS (
+            SELECT ? AS logdate
+            UNION ALL
+            SELECT logdate + INTERVAL 1 DAY FROM dates WHERE logdate < ?
+          )
+          SELECT 
+              a.employee_qrcode, 
+              b.employee_idno, 
+              b.first_name, 
+              b.last_name, 
+              COALESCE(MIN(a.time_in), '') AS min_time_in, 
+              COALESCE(MAX(a.time_out), '') AS max_time_out, 
+              d.logdate AS logdate
+          FROM dates d
+          CROSS JOIN tbl_employee b
+          LEFT JOIN tbl_attendance a 
+              ON a.employee_qrcode = b.qr_code 
+              AND a.logdate = d.logdate
+          GROUP BY a.employee_qrcode, b.employee_idno, b.first_name, b.last_name, d.logdate
+          ORDER BY b.last_name ASC, b.first_name ASC, d.logdate ASC
+          ");
         $stmt->bind_param("ss", $date1, $date2);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -57,26 +51,18 @@
             $time_out = $fetch['max_time_out'];
 
             // Add the row to the output with the earliest time in and latest time out
-            $output .= "
-                <tr>
-                    <td>".$fetch['employee_qrcode']."</td>
-                    <td>".$fetch['employee_idno']."</td>
-                    <td>".$fetch['first_name']."</td>
-                    <td>".$fetch['last_name']."</td>
-                    <td>".$time_in."</td>
-                    <td>".$time_out."</td>
-                    <td>".date("M d, Y", strtotime($fetch['logdate']))."</td>
-                </tr>
-            ";
+            $output .= 
+                $fetch['employee_qrcode'] . ',' .
+                $fetch['employee_idno'] . ',' .
+                $fetch['first_name'] . ',' .
+                $fetch['last_name'] . ',' .
+                $time_in . ',' .
+                $time_out . ',' .
+                date("M d, Y", strtotime($fetch['logdate'])) .
+                "\n";
         }
 
-        // Close the table
-        $output .= "
-                </tbody>
-            </table>
-        ";
-
-        // Output the completed table to the browser as an Excel file
+        // Output the completed table to the browser as a CSV file
         echo $output;
     }
 ?>
